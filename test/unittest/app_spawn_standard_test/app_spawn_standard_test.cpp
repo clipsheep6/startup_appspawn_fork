a182ea7411da3b9fb2bf30633061e7a9e5dcb5be
laiguizhong <laiguizhong@huawei.com>
2022-06-06 19:17:56 +0800

    修改测试用例及编译告错
    Signed-off-by: laiguizhong <laiguizhong@huawei.com>

--- test/unittest/app_spawn_standard_test/app_spawn_standard_test.cpp
+++ test/unittest/app_spawn_standard_test/app_spawn_standard_test.cpp
@@ -31,6 +31,69 @@
 using namespace testing;
 using namespace testing::ext;
 
+#ifdef __cplusplus
+    extern "C" {
+#endif
+    int OnConnection(const LoopHandle loopHandle, const TaskHandle server);
+    void AppSpawnRun(AppSpawnContent *content, int argc, char *const argv[]);
+#ifdef __cplusplus
+    }
+#endif
+
+static void runChildProcessor(struct AppSpawnContent_ *content, AppSpawnClient *client)
+{}
+
+static void initAppSpawn(struct AppSpawnContent_ *content)
+{}
+
+static void loadExtendLib(struct AppSpawnContent_ *content)
+{}
+
+static void runAppSpawn(struct AppSpawnContent_ *content, int argc, char *const argv[])
+{}
+
+static void clearEnvironment(struct AppSpawnContent_ *content, AppSpawnClient *client)
+{}
+
+static void setAppAccessToken(struct AppSpawnContent_ *content, AppSpawnClient *client)
+{}
+
+static int setAppSandbox(struct AppSpawnContent_ *content, AppSpawnClient *client)
+{
+    return 0;
+}
+
+static int setKeepCapabilities(struct AppSpawnContent_ *content, AppSpawnClient *client)
+{
+    return 0;
+}
+
+static int setFileDescriptors(struct AppSpawnContent_ *content, AppSpawnClient *client)
+{
+    return 0;
+}
+
+static int setProcessName(struct AppSpawnContent_ *content, AppSpawnClient *client,
+    char *longProcName, uint32_t longProcNameLen)
+{
+    return 0;
+}
+
+static int setUidGid(struct AppSpawnContent_ *content, AppSpawnClient *client)
+{
+    return 0;
+}
+
+static int setCapabilities(struct AppSpawnContent_ *content, AppSpawnClient *client)
+{
+    return 0;
+}
+
+static int notifyResToParent(struct AppSpawnContent_ *content, AppSpawnClient *client, int result)
+{
+    return 0;
+}
+
 namespace OHOS {
 class AppSpawnStandardTest : public testing::Test {
 public:
@@ -55,25 +118,21 @@ void AppSpawnStandardTest::TearDown()
 HWTEST(AppSpawnStandardTest, App_Spawn_Standard_001, TestSize.Level0)
 {
     GTEST_LOG_(INFO) << "App_Spawn_Standard_001 start";
-    string longProcName = "AppSpawnStandardTest1";
+    string longProcName = "ohos.samples.ecg.default";
     int64_t longProcNameLen = longProcName.length();
-    int cold = 1;
+    int cold = 0;
     AppSpawnContent *content = AppSpawnCreateContent("AppSpawn", (char*)longProcName.c_str(), longProcNameLen, cold);
     EXPECT_TRUE(content);
     content->loadExtendLib = LoadExtendLib;
     content->runChildProcessor = RunChildProcessor;
-
-    char *const argv[] = {};
-    content->initAppSpawn(content);
-    AppSpawnColdRun(content, 1, argv);
-
+    AppSpawnRun(content, 0, nullptr);
     GTEST_LOG_(INFO) << "App_Spawn_Standard_001 end";
 }
 
 HWTEST(AppSpawnStandardTest, App_Spawn_Standard_002, TestSize.Level0)
 {
     GTEST_LOG_(INFO) << "App_Spawn_Standard_002 start";
-    string longProcName = "AppSpawnStandardTest2";
+    string longProcName = "ohos.samples.ecg.default";
     int64_t longProcNameLen = longProcName.length();
 
     AppSpawnClientExt* client = (AppSpawnClientExt*)malloc(sizeof(AppSpawnClientExt));
@@ -82,12 +141,16 @@ HWTEST(AppSpawnStandardTest, App_Spawn_Standard_002, TestSize.Level0)
     if (strcpy_s(client->property.apl, APP_APL_MAX_LEN, "system_basic") != 0) {
         GTEST_LOG_(INFO) << "strcpy_s failed";
     }
-    pid_t pid = 100;
-    AppSpawnContentExt* appSpawnContent = (AppSpawnContentExt*)calloc(1, sizeof(AppSpawnContentExt));
+
+    pid_t pid = 0;
+    AppSpawnContentExt* appSpawnContent = (AppSpawnContentExt*)malloc(sizeof(AppSpawnContentExt));
     EXPECT_TRUE(appSpawnContent);
+    if (strcpy_s(appSpawnContent->content.longProcName, longProcNameLen, longProcName.c_str()) != 0) {
+        GTEST_LOG_(INFO) << "strcpy_s failed";
+    };
     appSpawnContent->content.longProcNameLen = longProcNameLen;
     appSpawnContent->timer = NULL;
-    appSpawnContent->content.runAppSpawn = NULL;
+    appSpawnContent->content.runAppSpawn = runAppSpawn;
     appSpawnContent->content.initAppSpawn = NULL;
     AppSpawnProcessMsg(&appSpawnContent->content, &client->client, &pid);
     free(appSpawnContent);
@@ -98,7 +161,7 @@ HWTEST(AppSpawnStandardTest, App_Spawn_Standard_002, TestSize.Level0)
 HWTEST(AppSpawnStandardTest, App_Spawn_Standard_003, TestSize.Level0)
 {
     GTEST_LOG_(INFO) << "App_Spawn_Standard_003 start";
-    char longProcName[124] = "AppSpawnStandardTest3";
+    char longProcName[124] = "ohos.samples.ecg.default";
     int64_t longProcNameLen = 124; // 124 is str length
     std::unique_ptr<AppSpawnClientExt> clientExt = std::make_unique<AppSpawnClientExt>();
 
@@ -139,6 +202,10 @@ HWTEST(AppSpawnStandardTest, App_Spawn_Standard_003, TestSize.Level0)
     content->setAppSandbox(content, &clientExt->client);
     content->setAppAccessToken(content, &clientExt->client);
     EXPECT_EQ(content->coldStartApp(content, &clientExt->client), 0);
+
+    int ret = ForkChildProc((AppSpawnContent_*)content, &clientExt->client, 0);
+    EXPECT_EQ(ret, 0);
+    ForkChildProc((AppSpawnContent_ *)content, &clientExt->client, -1);
     DoStartApp(content, &clientExt->client, longProcName, longProcNameLen);
     free(content);
     GTEST_LOG_(INFO) << "App_Spawn_Standard_003 end";
@@ -147,22 +214,21 @@ HWTEST(AppSpawnStandardTest, App_Spawn_Standard_003, TestSize.Level0)
 HWTEST(AppSpawnStandardTest, App_Spawn_Standard_004, TestSize.Level0)
 {
     GTEST_LOG_(INFO) << "App_Spawn_Standard_004 start";
-
     AppSpawnClientExt* client = (AppSpawnClientExt*)malloc(sizeof(AppSpawnClientExt));
-    client->client.id = 1;
-    client->client.flags = 0;
+    client->client.id = 8;
+    client->client.flags = 1;
     client->fd[0] = 1;
     client->fd[1] = 2;
     client->property.uid = 10000;
     client->property.gid = 1000;
     client->property.gidCount = 1;
-    if (strcpy_s(client->property.processName, APP_LEN_PROC_NAME, "test4") != 0) {
+    if (strcpy_s(client->property.processName, APP_LEN_PROC_NAME, "ohos.samples.ecg") != 0) {
         GTEST_LOG_(INFO) << "strcpy_s failed";
     }
-    if (strcpy_s(client->property.bundleName, APP_LEN_BUNDLE_NAME, "test4") != 0) {
+    if (strcpy_s(client->property.bundleName, APP_LEN_BUNDLE_NAME, "ohos.samples.ecg") != 0) {
         GTEST_LOG_(INFO) << "strcpy_s failed";
     }
-    if (strcpy_s(client->property.soPath, APP_LEN_SO_PATH, "test4") != 0) {
+    if (strcpy_s(client->property.soPath, APP_LEN_SO_PATH, "default") != 0) {
         GTEST_LOG_(INFO) << "strcpy_s failed";
     }
     client->property.accessTokenId = 671201800;
@@ -174,11 +240,100 @@ HWTEST(AppSpawnStandardTest, App_Spawn_Standard_004, TestSize.Level0)
     }
     client->property.flags = 0;
 
-    char* argv[] = {const_cast<char*>("AppSpawnStandardTest4"), const_cast<char*>("test4")};
+    char* argv[] = {const_cast<char*>("default"), const_cast<char*>("ohos.samples.ecg")};
     int argc = sizeof(argv)/sizeof(argv[0]);
 
     EXPECT_EQ(GetAppSpawnClientFromArg(argc, argv, client), -1);
     free(client);
     GTEST_LOG_(INFO) << "App_Spawn_Standard_004 end";
+}
+
+HWTEST(AppSpawnStandardTest, App_Spawn_Standard_005, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "App_Spawn_Standard_005 start";
+    string longProcName = "ohos.samples.ecg.default";
+    int64_t longProcNameLen = longProcName.length();
+
+    AppSpawnClientExt* client = (AppSpawnClientExt*)malloc(sizeof(AppSpawnClientExt));
+    client->client.id = 20010013;
+    client->client.flags = 0;
+    if (strcpy_s(client->property.apl, APP_APL_MAX_LEN, "system_basic") != 0) {
+        GTEST_LOG_(INFO) << "strcpy_s failed";
+    }
+    pid_t pid = 100;
+    AppSpawnContentExt* appSpawnContent = (AppSpawnContentExt*)malloc(sizeof(AppSpawnContentExt));
+    EXPECT_TRUE(appSpawnContent);
+    if (strcpy_s(appSpawnContent->content.longProcName, longProcNameLen, longProcName.c_str()) != 0) {
+        GTEST_LOG_(INFO) << "strcpy_s failed";
+    };
+    appSpawnContent->content.longProcNameLen = longProcNameLen;
+    appSpawnContent->timer = NULL;
+    appSpawnContent->content.runAppSpawn = NULL;
+    appSpawnContent->content.initAppSpawn = NULL;
+    AppSpawnProcessMsg(&appSpawnContent->content, &client->client, &pid);
+    free(appSpawnContent);
+    free(client);
+    GTEST_LOG_(INFO) << "App_Spawn_Standard_005 end";
+}
+
+HWTEST(AppSpawnStandardTest, App_Spawn_Standard_006, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "App_Spawn_Standard_006 start";
+    string longProcName = "ohos.samples.ecg.default";
+    int64_t longProcNameLen = longProcName.length();
+    int cold = 1;
+    AppSpawnContent *content = AppSpawnCreateContent("AppSpawn", (char*)longProcName.c_str(), longProcNameLen, cold);
+    EXPECT_TRUE(content);
+    content->loadExtendLib = LoadExtendLib;
+    content->runChildProcessor = RunChildProcessor;
+
+    char tmp0[] = "/system/bin/appspawn";
+    char tmp1[] = "cold-start";
+    char tmp2[] = "1";
+    char tmp3[] = "1:1:1:1:0:ohos.samples.ecg.default:ohos.samples.ecg:default:671201800:system_core:default";
+    char * const argv[] = {tmp0, tmp1, tmp2, tmp3};
+    content->initAppSpawn(content);
+    AppSpawnColdRun(content, 4, argv);
+    GTEST_LOG_(INFO) << "App_Spawn_Standard_006 end";
+}
+
+HWTEST(AppSpawnStandardTest, App_Spawn_Standard_007, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "App_Spawn_Standard_007 start";
+    RunChildProcessor(nullptr, nullptr);
+    GTEST_LOG_(INFO) << "App_Spawn_Standard_007 end";
+}
+
+HWTEST(AppSpawnStandardTest, App_Spawn_Standard_008, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "App_Spawn_Standard_008 start";
+    string longProcName = "ohos.samples.ecg.default";
+    int64_t longProcNameLen = longProcName.length();
+    std::unique_ptr<AppSpawnClientExt> clientExt = std::make_unique<AppSpawnClientExt>();
+    AppSpawnContent *content = AppSpawnCreateContent("AppSpawn", (char*)longProcName.c_str(), longProcNameLen, 1);
+    content->setAppSandbox = setAppSandbox;
+    content->setKeepCapabilities = setKeepCapabilities;
+    content->setProcessName = setProcessName;
+    content->setUidGid = setUidGid;
+    content->setFileDescriptors = setFileDescriptors;
+    content->setCapabilities = setCapabilities;
+    int ret = DoStartApp((AppSpawnContent_*)content, &clientExt->client, (char*)"", 0);
+    EXPECT_EQ(ret, 0);
+    GTEST_LOG_(INFO) << "App_Spawn_Standard_008 end";
+}
+
+HWTEST(AppSpawnStandardTest, App_Spawn_Standard_009, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "App_Spawn_Standard_009 start";
+    TaskHandle server = (TaskHandle)malloc(sizeof(TaskHandle));
+    EXPECT_TRUE(server);
+    int ret = OnConnection(nullptr, server);
+    EXPECT_EQ(ret, -1);
+    server->flags = 0;
+    OnConnection(nullptr, server);
+    server->flags = 1;
+    OnConnection(nullptr, server);
+    free(server);
+    GTEST_LOG_(INFO) << "App_Spawn_Standard_009 end";
 }
 } // namespace OHOS
